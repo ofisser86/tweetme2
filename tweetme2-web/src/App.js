b50382c5b6af0c0b5d4f576c975c3918f09ae670
Didi <ofisser86@gmail.com>
2020-06-01 12:39:24 +0300

    Add load tweet compnent is App js

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+        function loadTweet(callback){
+            const xhr = new XMLHttpRequest()
+            const method = 'GET'
+            const url = 'http://localhost:8000/api/tweets'
+            const responseType = 'json'
+            xhr.responseType = responseType
+            xhr.open(method, url)
+
+            xhr.onload = function () {
+                callback(xhr.response, xhr.status)
+            }
+            xhr.onerror= function(e){
+              console.log('----', e)
+              callback({'message': "The request was an error"}, 400)
+
+            }
+            xhr.send()
+        }
+
+
 function App() {
+  const [tweets, setTweets] = useState([])
+  useEffect(() => {
+    // Todo: to do my looks up
+    const myCallBack = (response, status) => {
+      console.log(response, status)
+      if (status === 200){
+         setTweets(response)
+      } else {
+        alert('There was an error')
+      }
+    }
+    loadTweet(myCallBack)
+
+
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {tweets.map((tweet, index)=>{
+            return <li>{tweet.content}</li>
+
+
+          })}
+
         </p>
         <a
           className="App-link"
