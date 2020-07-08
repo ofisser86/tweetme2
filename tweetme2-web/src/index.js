94b7e43d2f4bc4e7af590841dcf06421766b877d
Didi <ofisser86@gmail.com>
2020-07-08 14:38:06 +0300

    Edit index.js from App. Run npm build

--- tweetme2-web/src/index.js
+++ tweetme2-web/src/index.js
@@ -2,14 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {TweetsComponent} from "./tweets";
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+
+const appEl = document.getElementById('root')
+if (appEl){
+    ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  appEl
 );
+}
+
+const tweetEl = document.getElementById('tweetMe-2')
+if (tweetEl){
+    ReactDOM.render(
+  <React.StrictMode>
+    <TweetsComponent />
+  </React.StrictMode>,
+  tweetEl
+);
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
