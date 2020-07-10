5221fff7464bb07f68054974537dd135cd4f5a09
Didi <ofisser86@gmail.com>
2020-07-10 16:59:52 +0300

    Set Data Props on ReactDOM Render

--- tweetme2-web/src/index.js
+++ tweetme2-web/src/index.js
@@ -2,31 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {TweetsComponent} from "./tweets";
-
+import {TweetsComponent} from './tweets'
 import * as serviceWorker from './serviceWorker';
 
-
 const appEl = document.getElementById('root')
-if (appEl){
-    ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  appEl
-);
+if (appEl) {
+    ReactDOM.render(<App />, appEl);
 }
-
-const tweetEl = document.getElementById('tweetMe-2')
-if (tweetEl){
+const e = React.createElement
+const tweetsEl = document.getElementById("tweetme-2")
+if (tweetsEl) {
     ReactDOM.render(
-  <React.StrictMode>
-    <TweetsComponent />
-  </React.StrictMode>,
-  tweetEl
-);
+        e(TweetsComponent, tweetsEl.dataset), tweetsEl);
 }
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
