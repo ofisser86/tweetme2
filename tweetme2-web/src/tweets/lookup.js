ebe1287a904549f40d1ce0f1dee66c24e15c9cea
Didi <ofisser86@gmail.com>
2020-07-10 17:20:33 +0300

    Rendering Limited Tweet List by Username

--- tweetme2-web/src/tweets/lookup.js
+++ tweetme2-web/src/tweets/lookup.js
@@ -1,14 +1,23 @@
-import {backendLookup} from "../lookup"
+import {backendLookup} from '../lookup'
 
-export function apiTweetCreate(newTweet, callback) {
-  backendLookup("POST", "/tweets/create/", callback, {content: newTweet})
+export function apiTweetCreate(newTweet, callback){
+    backendLookup("POST", "/tweets/create/", callback, {content: newTweet})
+  }
+
+export function apiTweetAction(tweetId, action, callback){
+    const data = {id: tweetId, action: action}
+    backendLookup("POST", "/tweets/action/", callback, data)
 }
 
-export function apiTweetAction(tweetId, action, callback) {
-  const data = {id: tweetId, 'action':action}
-  backendLookup("POST", "/tweets/action/", callback, data)
+export function apiTweetDetial(tweetId, callback) {
+    backendLookup("GET", `/tweets/${tweetId}/`, callback)
 }
 
-export function apiTweetList(callback) {
-    backendLookup("GET", "/tweets/", callback)
+
+export function apiTweetList(username, callback) {
+    let endpoint =  "/tweets/"
+    if (username){
+        endpoint =  `/tweets/?username=${username}`
+    }
+    backendLookup("GET", endpoint, callback)
 }
