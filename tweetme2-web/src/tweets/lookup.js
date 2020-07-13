4949d8b28b5f5a16cbcf2e7b01ab7001e9652969
Didi <ofisser86@gmail.com>
2020-07-13 16:00:15 +0300

    Lookup & Render & Embed Individual Tweets

--- tweetme2-web/src/tweets/lookup.js
+++ tweetme2-web/src/tweets/lookup.js
@@ -9,10 +9,9 @@ export function apiTweetAction(tweetId, action, callback){
     backendLookup("POST", "/tweets/action/", callback, data)
 }
 
-export function apiTweetDetial(tweetId, callback) {
+export function apiTweetDetail(tweetId, callback) {
     backendLookup("GET", `/tweets/${tweetId}/`, callback)
 }
-
 
 export function apiTweetList(username, callback) {
     let endpoint =  "/tweets/"
