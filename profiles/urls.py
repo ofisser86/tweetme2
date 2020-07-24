3560b93c181521f1916439db09de5e09bba3ac87
Didi <ofisser86@gmail.com>
2020-07-24 15:12:35 +0300

    Removing Redundant Profile View

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-
-from .views import profile_detail_view, profile_update_view
-
+from .views import (
+    profile_detail_api_view,
+)
+'''
+CLIENT
+Base ENDPOINT /api/profiles/
+'''
 urlpatterns = [
-    path('edit', profile_update_view),
-    path('<str:username>', profile_detail_view),
+    path('<str:username>/', profile_detail_api_view),
+    path('<str:username>/follow', profile_detail_api_view),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,55 +1,70 @@
-from django.http import Http404
+import random
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render, redirect
+from django.utils.http import is_safe_url
 
-from .forms import ProfileForm
-from .models import Profile
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from ..models import Profile
+from ..serializers import PublicProfileSerializer
 
+User = get_user_model()
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
-
-def profile_update_view(request, *args, **kwargs):
-    if not request.user.is_authenticated: # is_authenticated()
-        return redirect("/login?next=/profile/update")
-    user = request.user
-    user_data = {
-        "first_name": user.first_name,
-        "last_name": user.last_name,
-        "email": user.email
-    }
-    my_profile = user.profile
-    form = ProfileForm(request.POST or None, instance=my_profile, initial=user_data)
-    if form.is_valid():
-        profile_obj = form.save(commit=False)
-        first_name = form.cleaned_data.get('first_name')
-        last_name = form.cleaned_data.get('last_name')
-        email = form.cleaned_data.get('email')
-        user.first_name = first_name
-        user.last_name = last_name
-        user.email = email
-        user.save()
-        profile_obj.save()
-    context = {
-        "form": form,
-        "btn_label": "Save",
-        "title": "Update Profile"
-    }
-    return render(request, "profiles/form.html", context)
+# @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+# def user_profile_detail_view(request, username, *args, **kwargs):
+#     current_user = request.user
+#     to_follow_user = ??
+#     return Response({}, status=200)
 
 
-
-def profile_detail_view(request, username, *args, **kwargs):
+@api_view(['GET', 'POST'])
+def profile_detail_api_view(request, username, *args, **kwargs):
     # get the profile for the passed username
     qs = Profile.objects.filter(user__username=username)
     if not qs.exists():
-        raise Http404
+        return Response({"detail": "User not found"}, status=404)
     profile_obj = qs.first()
-    is_following = False
-    if request.user.is_authenticated:
-        user = request.user
-        is_following = user in profile_obj.followers.all()
-        # is_following = profile_obj in user.following.all()
-    context = {
-        "username": username,
-        "profile": profile_obj,
-        "is_following": is_following
-    }
-    return render(request, "profiles/detail.html", context)
+    data = request.data or {}
+    if request.method == "POST":
+        me = request.user
+        action = data.get("action")
+        if profile_obj.user != me:
+            if action == "follow":
+                profile_obj.followers.add(me)
+            elif action == "unfollow":
+                profile_obj.followers.remove(me)
+            else:
+                pass
+    serializer = PublicProfileSerializer(instance=profile_obj, context={"request": request})
+    return Response(serializer.data, status=200)
+
+
+# @api_view(['GET', 'POST'])
+# @permission_classes([IsAuthenticated])
+# def user_follow_view(request, username, *args, **kwargs):
+#     me = request.user
+#     other_user_qs = User.objects.filter(username=username)
+#     if me.username == username:
+#         my_followers = me.profile.followers.all()
+#         return Response({"count": my_followers.count()}, status=200)
+#     if not other_user_qs.exists():
+#         return Response({}, status=404)
+#     other = other_user_qs.first()
+#     profile = other.profile
+#     data = request.data or {}
+#     action = data.get("action")
+#     if action == "follow":
+#         profile.followers.add(me)
+#     elif action == "unfollow":
+#         profile.followers.remove(me)
+#     else:
+#         pass
+#     data = PublicProfileSerializer(instance=profile, context={"request": request})
+#     return Response(data.data, status=200)
